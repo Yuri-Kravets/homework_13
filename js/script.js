6b72517dc239cf0bf396857a2d3dc0c89594050a
Yuri-Kravets <91874641+Yuri-Kravets@users.noreply.github.com>
2021-11-03 11:15:41 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 
 function getDegree(num, degree = 1) {
-    if (num.trim() === '' || isNaN(num) || num === null ) return ('some error');
-    if (degree.trim() === '' || isNaN(degree) || degree === null ) return ('some error');
-    
+    //if (num.trim() === '' || isNaN(num) || num === null ) return ('some error');
+    //if (degree.trim() === '' || isNaN(degree) || degree === null ) return ('some error');
+    if (typeof num !== 'number' && isNaN(num)) return ('some error');
+    if (typeof degree !== 'number' && isNaN(degree)) return ('some error');
     return alert(Math.pow(num,degree));
     
 }
